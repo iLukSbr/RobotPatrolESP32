7d3f2402c9890de136de09d7f2b7c10ed79177e4
iLukSbr <lucasyfm@hotmail.com>
2024-12-04 11:09:11 -0300

    boot

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import machine
-import time
+# import machine
+# import time
 
-# Initialize the LED pin (assuming the LED is connected to pin 2)
-led = machine.Pin(2, machine.Pin.OUT)
+# # Initialize the LED pin (assuming the LED is connected to pin 2)
+# led = machine.Pin(2, machine.Pin.OUT)
 
-# Print "Hello, World!" to the serial monitor
-print("Hello, World!")
+# # Print "Hello, World!" to the serial monitor
+# print("Hello, World!")
 
-# Blink the LED
-while True:
-    led.on()
-    time.sleep(1)  # LED on for 1 second
-    led.off()
-    time.sleep(1)  # LED off for 1 second
+# # Blink the LED
+# while True:
+#     led.on()
+#     time.sleep(1)  # LED on for 1 second
+#     led.off()
+#     time.sleep(1)  # LED off for 1 second
